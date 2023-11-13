40765c196a0ae7efea76767f8ae6076ee4e8cba3
EmersonSalcedo <emersondavid501@gmail.com>
2023-11-12 21:01:50 -0500

    cambiando a flowbite

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   mostrarFormulario:Boolean=false;
   cambiar:Boolean=false;
+
+  cerrarFormulario(cerrar:boolean) {
+    this.mostrarFormulario=cerrar;
+  }
 }
